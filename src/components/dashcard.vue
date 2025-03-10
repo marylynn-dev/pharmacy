2dd85e890381b3f29f23b67f96bb65fe6521063d
Marylynn <marlymaingi28@gmail.com>
2025-03-10 22:28:49 +0300

    dashboard cards

---
+++ src/components/dashcard.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card class="text-center d-flex flex-column" height="180px">
+    <v-card-text class="flex-grow-1 d-flex flex-column justify-center">
+      <div class="d-flex align-center justify-center" style="height: 60px">
+        <v-icon :color="card.color" size="38px">{{ card.icon }}</v-icon>
+      </div>
+      <p class="font-weight-bold">{{ card.text }}</p>
+      <p>{{ card.subtext }}</p>
+    </v-card-text>
+
+    <v-btn
+      append-icon="mdi-chevron-right"
+      :color="card.color"
+      :text="card.action"
+      variant="flat"
+      block
+      rounded="0"
+      class="mt-auto"
+    ></v-btn>
+  </v-card>
+</template>
+
+<script setup>
+defineProps({
+  card: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,11 +4,47 @@
       title="Dashboard"
       subtitle="A quick data overview of the inventory"
     />
+    <v-row>
+      <v-col v-for="card in cardData" cols="3">
+        <dashcard :card="card" />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script setup>
 import PageHeader from "@/components/PageHeader.vue";
+
+const cardData = [
+  {
+    text: "Good",
+    action: "View Report",
+    subtext: "Inventory Status",
+    icon: "mdi-check-circle-outline",
+    color: "green",
+  },
+  {
+    text: "Ksh 2000000",
+    action: "Reorder Now",
+    subtext: "Revenue: Jan 2022",
+    icon: "mdi-alert-circle-outline",
+    color: "orange",
+  },
+  {
+    text: "298",
+    subtext: "Medicines Available",
+    action: "Remove Expired Items",
+    icon: "mdi-medical-bag",
+    color: "red",
+  },
+  {
+    text: "01",
+    subtext: "Medicine Shortage",
+    action: "Review Stock",
+    icon: "mdi-plus-circle-outline",
+    color: "blue",
+  },
+];
 
 //
 </script>
